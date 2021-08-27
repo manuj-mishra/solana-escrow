2fce6eb402b829896668d3025171aad17504c56f
Manuj Mishra <mm4519@ic.ac.uk>
2021-08-27 08:34:27 +0100

    implement InitEscrow instruction with custom EscrowError

---
+++ src/instruction.rs
@@ -0,0 +1,45 @@
+use std::convert::TryInto;
+use solana_program::program_error::ProgramError;
+
+use crate::error::EscrowError::InvalidInstruction;
+
+pub enum EscrowInstruction {
+    /// Starts the trade by creating and populating an escrow account and transferring ownership of the given temp token account to the PDA
+    ///
+    ///
+    /// Accounts expected:
+    ///
+    /// 0. `[signer]` Account of the person initializing the escrow
+    /// 1. `[writable]` Initializer's temporary token account for the token they are offering (token X)
+    /// 2. `[]` Initializer's token account for the token they will receive (token Y) should the trade go through
+    /// 3. `[writable]` The escrow account, it will hold all necessary info about the trade.
+    /// 4. `[]` The rent sysvar
+    /// 5. `[]` The token program
+    InitEscrow {
+        /// The amount the initializer expects to receive of token Y
+        amount: u64
+    }
+}
+
+impl EscrowInstruction {
+    /// Unpacks a byte buffer into a [EscrowInstruction](enum.EscrowInstruction.html).
+    pub fn unpack(input: &[u8]) -> Result<Self, ProgramError> {
+        let (tag, rest) = input.split_first().ok_or(InvalidInstruction)?;
+
+        Ok(match tag {
+            0 => Self::InitEscrow {
+                amount: Self::unpack_amount(rest)?,
+            },
+            _ => return Err(InvalidInstruction.into()),
+        })
+    }
+
+    fn unpack_amount(input: &[u8]) -> Result<u64, ProgramError> {
+        let amount = input
+            .get(..8)
+            .and_then(|slice| slice.try_into().ok())
+            .map(u64::from_le_bytes)
+            .ok_or(InvalidInstruction)?;
+        Ok(amount)
+    }
+}
